f0ddf1c43ffe6c6136e6c9a4de7ea11a882454ea
Sumit Pawar <sumitpawar1991@gmail.com>
2026-06-01 23:24:50 +0530

    Convert client submodule to regular directory

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import {Col,Container,Row} from "react-bootstrap"
+
+function App() {
+  return (
+  <Container>
+    <Row>
+      <Col xs="12 py-2">
+        <h1 className="text-center">
+            React application with fiber backend
+        </h1>
+      </Col>
+    </Row>
+  </Container>
+  );
+}
+
+export default App;
